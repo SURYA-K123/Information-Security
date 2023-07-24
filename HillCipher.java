95fb0a936856bcdfe2c71839f6e701c9f27b38d1
SURYA <102934593+SURYA-K123@users.noreply.github.com>
2023-07-24 22:25:41 +0530

    Update HillCipher.java

--- HillCipher.java
+++ HillCipher.java
@@ -21,6 +21,54 @@ public class HillCipher {
         }
         return matrix;
     }
+    int[][] adjointMatrix(int mat[][]) {
+        int length = mat.length;
+        boolean isNegative = false;
+        int adj[][] = new int[length][length];
+        if(mat.length==2)
+        {
+            adj[0][0] = getMod(mat[1][1]);
+            adj[1][1] = getMod(mat[0][0]);
+            adj[0][1] = getMod(mat[0][1]*(-1));
+            adj[1][0] = getMod(mat[1][0]*(-1));
+            return adj;
+        }
+        for (int i = 0; i < length; i++) {
+
+            for (int j = 0; j < length; j++) {
+
+                int subMat[][] = new int[length - 1][length - 1];
+                int row = 0;
+
+                for (int k = 0; k < length; k++) {
+                    int column = 0;
+                    if (k == i) {
+                        continue;
+                    } else {
+                        for (int l = 0; l < length; l++) {
+                            if (l == j) {
+                                continue;
+                            } else {
+                                subMat[row][column] = mat[k][l];
+                                column++;
+                            }
+                        }
+                    }
+                    row++;
+                }
+                if (!isNegative) {
+                    adj[j][i] = getMod(determinant(subMat));
+                    isNegative = !isNegative;
+                } else {
+                    adj[j][i] = getMod((-1) * determinant(subMat));
+                    isNegative = !isNegative;
+                }
+
+            }
+            isNegative = !isNegative;
+        }
+        return adj;
+    }
     int[][] matrixMultiplication(int mat1[][],int mat2[][])
     {
         int multipliedMatrix[][] = new int[mat1.length][mat2[0].length];
